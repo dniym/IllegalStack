1230b5d9739d626804488e0b091a9a96fce157d8
Sascha Bartl <bartl.sascha0@gmail.com>
2023-06-10 10:34:03 +0200

    Add MC 1.20 Support

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,15 +38,15 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.19.2-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
     compileOnly("com.comphenix.protocol:ProtocolLib:4.7.0-SNAPSHOT")
     implementation("com.elmakers.mine.bukkit:MagicAPI:8.6")
     implementation("de.tr7zw:item-nbt-api-plugin:2.8.0")
     implementation("com.github.TheBusyBiscuit:Slimefun4:RC-30")
-    implementation("io.netty:netty-all:4.1.65.Final")
+    implementation("io.netty:netty-all:4.1.86.Final")
     implementation("com.gmail.nossr50.mcMMO:mcMMO:2.1.200") { isTransitive = false }
-    
-    
+
+
     implementation("fr.minuskube.inv:smart-invs:1.2.7")
     implementation("com.github.CraftingStore:MinecraftPlugin:master-SNAPSHOT")
     compileOnly(":JetsMinions")
@@ -67,7 +67,7 @@ tasks.compileJava.configure {
     options.release.set(8)
 }
 
-version = "2.7.1"
+version = "2.7.2"
 
 tasks.named<Copy>("processResources") {
     filesMatching("plugin.yml") {
--- src/main/java/main/java/me/dniym/enums/Protections.java
+++ src/main/java/main/java/me/dniym/enums/Protections.java
@@ -671,7 +671,7 @@ public enum Protections {
             8,
             true,
             "Destroy Rail / Carpet Dupers",
-            "1.12/1.13/1.14/1.15/1.16/1.17/1.18/1.19",
+            "1.12/1.13/1.14/1.15/1.16/1.17/1.18/1.19/1.20",
             "Exploits.Other.PreventRailDupe",
             "Prevent redstone machines designed to dupe carpets and rails, these items are usually duped to provide infinite fuel for furnaces or to sell for in game money in shops.",
             "",
@@ -953,7 +953,7 @@ public enum Protections {
             19,
             true,
             "Prevent Villager Trade Cheesing",
-            "1.14 / 1.15 / 1.16 / 1.17 / 1.18 / 1.19",
+            "1.14 / 1.15 / 1.16 / 1.17 / 1.18 / 1.19 / 1.20",
             "Exploits.1_14_Exploits.Traders.BlockVillagerTradeCheesing",
             "Prevents players from placing / breaking a villagers work station over and over which forces them to get new trades, typically people abuse this to make sure they get specific enchantments or items from a villager rather than it being a random mechanic.",
             "",
@@ -995,7 +995,7 @@ public enum Protections {
             46,
             true,
             "Prevent Tripwire Dupe",
-            "1.15 / 1.16 / 1.17 / 1.18 / 1.19",
+            "1.15 / 1.16 / 1.17 / 1.18 / 1.19 / 1.20",
             "Exploits.1_15_Exploits.Dupes.PreventTripwireDupe",
             "Prevents players from using trapdoors to dupe tripwire hooks.",
             "",
@@ -1042,7 +1042,7 @@ public enum Protections {
     		"Options for resetting spawners of a specific type",
     		"",
     		2
-    		
+
     ),
     ResetSpawnersOfType(
             33,
@@ -1066,7 +1066,7 @@ public enum Protections {
     		2,
     		false
     		),
-   
+
     RemoveItemsMatchingName(
             23,
             false,
@@ -1641,6 +1641,10 @@ public enum Protections {
             return false; //must be a child node
         }
 
+        if (this.getVersion().contains("< 1.20")) {
+            return !serverVersion.contains("1.20");
+        }
+
         if (this.getVersion().contains("< 1.19")) {
             return !serverVersion.contains("1.19");
         }
@@ -1655,7 +1659,7 @@ public enum Protections {
 
         if (this.getVersion().contains("< 1.15")) {
             return !serverVersion.contains("1.15") && !serverVersion.contains("1.16") && !serverVersion.contains("1.17") && !serverVersion.contains("1.18")
-            		&& !serverVersion.contains("1.19");
+            		&& !serverVersion.contains("1.19") && !serverVersion.contains("1.20");
         }
 
         if (this.getVersion().equalsIgnoreCase("1.14.4") && !serverVersion.contains("1.14.R4")) {
@@ -1667,6 +1671,10 @@ public enum Protections {
         }
 
         if (this.getVersion().contains("ALL")) {
+            return true;
+        }
+
+        if (this.getVersion().equalsIgnoreCase("1.20") && serverVersion.contains("1.20")) {
             return true;
         }
 
@@ -1718,15 +1726,19 @@ public enum Protections {
             return true;
         }
 
+        if (this.getVersion().contains("1.20") && serverVersion.contains("1.20")) {
+            return true;
+        }
+
         if (this.getVersion().contains("> 1.12")) {
-            if (serverVersion.contains("1.19") || serverVersion.contains("1.18") || serverVersion.contains("1.17") || serverVersion.contains("1.16") || serverVersion.contains("1.15") || serverVersion
+            if (serverVersion.contains("1.20") || serverVersion.contains("1.19") || serverVersion.contains("1.18") || serverVersion.contains("1.17") || serverVersion.contains("1.16") || serverVersion.contains("1.15") || serverVersion
                     .contains("1.14") || serverVersion.contains("1.13") || serverVersion.contains("1.12")) {
                 return true;
             }
         }
 
         if (this.getVersion().contains("> 1.9")) {
-            if (serverVersion.contains("1.19") || serverVersion.contains("1.18") || serverVersion.contains("1.17") || serverVersion.contains("1.16") || serverVersion.contains("1.15") || serverVersion
+            if (serverVersion.contains("1.20") || serverVersion.contains("1.19") || serverVersion.contains("1.18") || serverVersion.contains("1.17") || serverVersion.contains("1.16") || serverVersion.contains("1.15") || serverVersion
                     .contains("1.14") || serverVersion.contains("1.13") || serverVersion.contains("1.12") || serverVersion.contains(
                     "1.11") ||
                     this.serverVersion.contains("1.10") || this.serverVersion.contains("1.9")) {
@@ -1735,7 +1747,7 @@ public enum Protections {
         }
 
         if (this.getVersion().contains("> 1.11")) {
-            if (serverVersion.contains("1.19") || serverVersion.contains("1.18") || serverVersion.contains("1.17") || serverVersion.contains("1.16") || serverVersion.contains("1.15") || serverVersion
+            if (serverVersion.contains("1.20") || serverVersion.contains("1.19") || serverVersion.contains("1.18") || serverVersion.contains("1.17") || serverVersion.contains("1.16") || serverVersion.contains("1.15") || serverVersion
                     .contains("1.14") || serverVersion.contains("1.13") || serverVersion.contains("1.12") || serverVersion.contains(
                     "1.11")) {
                 return true;
@@ -2179,7 +2191,7 @@ public enum Protections {
     public boolean isWhitelisted(String name) {
         return isWhitelisted(name, null);
     }
-    
+
     public boolean isWhitelisted(EntityType et) {
     	if (this == Protections.ResetSpawnersOfTypeOnSpawn && et != null)
     	{
@@ -2189,7 +2201,7 @@ public enum Protections {
 
     	}
     	return false;
-    	
+
     }
     public boolean isWhitelisted(String name, Player player) {
 
@@ -2403,11 +2415,11 @@ public enum Protections {
             boolean minion = MinionAPI.isMinion(entity);
             if (minion) {
                 return true;
-            } 
+            }
         }
         if(entity instanceof LivingEntity && SpigotMethods.isNPC((LivingEntity)entity))
         	return true;
-        
+
         return false;
     }
 
