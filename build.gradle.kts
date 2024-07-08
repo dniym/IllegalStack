78d5135c9b08974a3cac5015079e78237fa12c2c
Loving11ish <loving11ish@gmail.com>
2024-07-08 21:13:10 +0100

    Added 1.21.0 support. Updated FoliaAPI to `1.20.4`. Added check for Folia server on startup. Fixed Folia shutdown scheduler error. Changed version to `2.9.12-SNAPSHOT-01`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("dev.folia:folia-api:1.20.2-R0.1-SNAPSHOT")
+    compileOnly("dev.folia:folia-api:1.20.4-R0.1-SNAPSHOT")
     compileOnly("com.comphenix.protocol:ProtocolLib:5.0.0")
     compileOnly("com.elmakers.mine.bukkit:MagicAPI:10.2")
     compileOnly("de.tr7zw:item-nbt-api-plugin:2.8.0")
@@ -66,7 +66,7 @@ tasks.compileJava.configure {
     options.release.set(8)
 }
 
-version = "2.9.11-SNAPSHOT-01"
+version = "2.9.12-SNAPSHOT-01"
 
 tasks.named<Copy>("processResources") {
     filesMatching("plugin.yml") {
--- src/main/java/main/java/me/dniym/enums/ServerVersion.java
+++ src/main/java/main/java/me/dniym/enums/ServerVersion.java
@@ -26,7 +26,9 @@ public enum ServerVersion {
     v1_20_R1,
     v1_20_R2,
     v1_20_R3,
-    v1_20_R4;
+    v1_20_R4,
+    v1_20_R5,
+    v1_21_R1;
 
     public boolean serverVersionEqual(ServerVersion version) {
         return this.equals(version);
